de78c82be7ce65799b9547068c8cb44b7f8e5b14
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-06-06 19:54:34 +0800

    fix: hn-linux-client: hook

--- pkgs/hn-linux-client/setup-hook.sh
+++ pkgs/hn-linux-client/setup-hook.sh
@@ -2,8 +2,8 @@
 # 设置客户端环境变量的钩子脚本
 postHook() {
     # 添加二进制目录到PATH
-    if [ -d "@out@/nix-support/bin" ]; then
-        export PATH="@out@/nix-support/bin:$PATH"
+    if [ -d "@out@/bin" ]; then
+        export PATH="@out@/bin:$PATH"
     fi
 
     # 设置应用特定环境变量（如果需要）
